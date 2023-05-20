01dd72521945d47dc67948d62d6d8087201cb003
Xenor <dawidxen@gmail.com>
2023-05-20 14:45:15 +0200

    Creates a new project structure

---
+++ PSNLibrary.cs
@@ -0,0 +1,87 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PSNLibrary
+{
+    public class PSNLibrary : LibraryPlugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private PSNLibrarySettingsViewModel settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("2ba68553-109e-48d5-b977-c4f8381e06f9");
+
+        // Change to something more appropriate
+        public override string Name => "Custom Library";
+
+        // Implementing Client adds ability to open it via special menu in playnite.
+        public override LibraryClient Client { get; } = new PSNLibraryClient();
+
+        public PSNLibrary(IPlayniteAPI api) : base(api)
+        {
+            settings = new PSNLibrarySettingsViewModel(this);
+            Properties = new LibraryPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public override IEnumerable<GameMetadata> GetGames(LibraryGetGamesArgs args)
+        {
+            // Return list of user's games.
+            return new List<GameMetadata>()
+            {
+                new GameMetadata()
+                {
+                    Name = "Notepad",
+                    GameId = "notepad",
+                    GameActions = new List<GameAction>
+                    {
+                        new GameAction()
+                        {
+                            Type = GameActionType.File,
+                            Path = "notepad.exe",
+                            IsPlayAction = true
+                        }
+                    },
+                    IsInstalled = true,
+                    Icon = new MetadataFile(@"c:\Windows\notepad.exe")
+                },
+                new GameMetadata()
+                {
+                    Name = "Calculator",
+                    GameId = "calc",
+                    GameActions = new List<GameAction>
+                    {
+                        new GameAction()
+                        {
+                            Type = GameActionType.File,
+                            Path = "calc.exe",
+                            IsPlayAction = true
+                        }
+                    },
+                    IsInstalled = true,
+                    Icon = new MetadataFile(@"https://playnite.link/applogo.png"),
+                    BackgroundImage = new MetadataFile(@"https://playnite.link/applogo.png")
+                }
+            };
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new PSNLibrarySettingsView();
+        }
+    }
+}
