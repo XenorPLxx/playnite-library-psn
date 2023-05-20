b585e7a9d81a694783894a811295df73ba12ffc3
Xenor <dawidxen@gmail.com>
2023-05-20 15:14:22 +0200

    Sets up the LibraryPlugin properties

--- PSNLibrary.cs
+++ PSNLibrary.cs
@@ -3,7 +3,9 @@ using Playnite.SDK.Models;
 using Playnite.SDK.Plugins;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
@@ -12,23 +14,29 @@ namespace PSNLibrary
 {
     public class PSNLibrary : LibraryPlugin
     {
+        // Playnite API access
+        private readonly INotificationsAPI notifications;
         private static readonly ILogger logger = LogManager.GetLogger();
 
+        // Settings access https://api.playnite.link/docs/master/tutorials/extensions/pluginSettings.html
         private PSNLibrarySettingsViewModel settings { get; set; }
 
-        public override Guid Id { get; } = Guid.Parse("2ba68553-109e-48d5-b977-c4f8381e06f9");
-
-        // Change to something more appropriate
-        public override string Name => "Custom Library";
-
-        // Implementing Client adds ability to open it via special menu in playnite.
+        // LibraryPlugin properties https://api.playnite.link/docs/master/api/Playnite.SDK.Plugins.LibraryPlugin.html#properties
+        public override Guid Id { get; } = Guid.Parse("e4ac81cb-1b1a-4ec9-8639-9a9633989a71");
         public override LibraryClient Client { get; } = new PSNLibraryClient();
+        public override string LibraryBackground => null;
+        public override string LibraryIcon => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"icon.png");
+        public override string Name => "PlayStation";
 
+        // LibraryPlugin constructor 
         public PSNLibrary(IPlayniteAPI api) : base(api)
         {
+            notifications = api.Notifications;
             settings = new PSNLibrarySettingsViewModel(this);
             Properties = new LibraryPluginProperties
             {
+                CanShutdownClient = false,
+                HasCustomizedGameImport = true,
                 HasSettings = true
             };
         }
