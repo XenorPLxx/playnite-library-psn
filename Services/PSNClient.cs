e8420112038fd849799375253eed9630dd811f00
Xenor <dawidxen@gmail.com>
2025-07-20 15:09:53 +0200

    Updates Playnite SDK

--- Services/PSNClient.cs
+++ Services/PSNClient.cs
@@ -59,10 +59,11 @@ namespace PSNLibrary.Services
         File.Delete(tokenPath);
       }
 
-      var webViewSettings = new WebViewSettings();
-      webViewSettings.WindowHeight = 700;
-      webViewSettings.WindowWidth = 580;
-      webViewSettings.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36";
+      WebViewSettings webViewSettings = new WebViewSettings();
+      webViewSettings.WindowHeight = 800;
+      webViewSettings.WindowWidth = 1100;
+      webViewSettings.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36";
+
       using (var view = api.WebViews.CreateView(webViewSettings))
       {
         view.LoadingChanged += (s, e) =>
